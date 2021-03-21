69fe174d836891ad672ddfb6ad4debc834d9560c
Niklas Krauss <nk.hemm@googlemail.com>
2021-03-21 12:40:42 +0100

    prepared inital experiment project structure

---
+++ project/experiment.py
@@ -0,0 +1,19 @@
+import json
+
+
+def start_experiment(config_path, log):
+    # print(config_path)
+    # print(log)
+    log_dir = log
+    config = ""
+
+    with open(config_path, mode="r", encoding="utf-8") as config_f:
+        config = json.load(config_f)
+
+    print(
+        """
+    **********************************************
+        EXPERIMENT DONE
+    **********************************************
+    """
+    )
---
+++ project/setup_experiment.py
@@ -0,0 +1,48 @@
+import os
+import argparse
+import sys
+import os
+
+PACKAGE_PARENT = ".."
+SCRIPT_DIR = os.path.dirname(
+    os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__)))
+)
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+
+# import shutil
+from project.experiment import start_experiment
+
+
+def main():
+    # Initiate argument parser
+    parser = argparse.ArgumentParser(
+        description="Preare run of AL with OoD experiment",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="Path to the config file for the experiment",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
+        "-l",
+        "--log",
+        help="Log folder",
+        type=str,
+        default=os.path.join("./logs"),
+    )
+    args = parser.parse_args()
+
+    print(args)
+
+    if args.config is None:
+        args.config = os.path.join(".\exp-config.json")
+
+    start_experiment(args.config, args.log)
+
+
+if __name__ == "__main__":
+    main()
