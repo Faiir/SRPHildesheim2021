539cea7e2b8cc682638a32809db945e976d0e6d5
Niklas Krauss <nk.hemm@googlemail.com>
2021-09-23 12:12:50 +0200

    stuff

--- project/ss_experiment.py
+++ project/ss_experiment.py
@@ -6,10 +6,21 @@ import torch.nn as nn
 import torchvision.transforms.functional as trnF
 import torchvision.datasets as dset
 import torch.nn.functional as F
-from .pertubed_dataset import create_pert_dataloader
-from ..model.resnet import add_rot_heads
-from ..model.get_model import get_model, save_model
-from ..data.datamanager import get_datamanager
+
+import sys
+import os
+
+PACKAGE_PARENT = ".."
+SCRIPT_DIR = os.path.dirname(
+    os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__)))
+)
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+
+from project.selfsupervision.pertubed_dataset import create_pert_dataloader
+from .model.resnet import add_rot_heads
+from .model.get_model import get_model, save_model
+from .data.datamanager import get_datamanager
 
 
 """ 
