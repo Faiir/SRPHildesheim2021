43376bb25056136bd5cbf2dfd4666fdb85ddd3b0
Faiir <59667141+Faiir@users.noreply.github.com>
2021-10-10 18:12:11 +0200

    manual changes

--- project/helpers/get_pool_predictions.py
+++ project/helpers/get_pool_predictions.py
@@ -24,9 +24,14 @@ def get_pool_predictions(trained_net, pool_loader, device, return_labels=False,b
     labels_list = []
     if trained_net.has_weighing_factor:
         print("Getting weight factor as well")
-
+    
     for (data, labels) in pool_loader:
         if trained_net.has_weighing_factor:
+            if bugged_and_working:
+                tuple_data = trained_net(data.to(device).float(), get_test_model=True, apply_softmax=False)
+            else:
+                tuple_data = trained_net(data.to(device).float(), get_test_model=True, apply_softmax=True)
+            pred = tuple_data[0]
             weighting_factor = tuple_data[1]
             weighting_factor_list.append(weighting_factor.to("cpu").detach().numpy())
         else:
@@ -39,7 +44,7 @@ def get_pool_predictions(trained_net, pool_loader, device, return_labels=False,b
         labels_list.append(labels)
 
     predictions = np.concatenate(yhat)
-    if len(weighting_factor_list) > 0:
+    if len(weighting_factor_list)>0:
         weighting_factor_list = np.concatenate(weighting_factor_list)
     else:
         weighting_factor_list = None
@@ -50,3 +55,4 @@ def get_pool_predictions(trained_net, pool_loader, device, return_labels=False,b
         return predictions, labels_list, weighting_factor_list
     else:
         return predictions, weighting_factor_list
+
