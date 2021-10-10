a18aa44ca54f118fc6ed73ae85f03b1080d5771c
AbdurRafae <m.abdur.rafae@gmail.com>
2021-10-10 13:40:25 +0000

    added flag to use old bugged version

--- project/experiment.py
+++ project/experiment.py
@@ -66,6 +66,12 @@ def experiment(param_dict, oracle, data_manager, writer, dataset, net):
     do_pertubed_images = param_dict["do_pertubed_images"]
     do_desity_plot = param_dict["do_desity_plot"]
     criterion = param_dict["criterion"]
+    if param_dict.get("bugged_and_working",None) is None:
+        bugged_and_working = param_dict.get("bugged_and_working",True)
+        print(f"INFO ---- flag bugged_and_working is not set. Using default value of {bugged_and_working}")
+    else:  
+        bugged_and_working = param_dict["bugged_and_working"]
+        print(f"INFO ---- flag bugged_and_working is set to {bugged_and_working}")
     
     OoD_extra_class = param_dict.get("OoD_extra_class", False)
     if OoD_extra_class:
@@ -197,7 +203,7 @@ def experiment(param_dict, oracle, data_manager, writer, dataset, net):
         )
         if do_desity_plot:
             pert_preds, gs, hs, targets = get_density_vals(
-                pool_loader, val_loader, trained_net, do_pertubed_images
+                pool_loader, val_loader, trained_net, do_pertubed_images, bugged_and_working
             )
 
             density_plot(pert_preds, gs, hs, targets, writer, i)
@@ -210,11 +216,16 @@ def experiment(param_dict, oracle, data_manager, writer, dataset, net):
             else:
                 # unlabelled pool predictions
                 pool_predictions, pool_labels_list, weighting_factors = get_pool_predictions(
-                trained_net, pool_loader, device=device, return_labels=True
+                trained_net, pool_loader, device=device, return_labels=True, bugged_and_working=bugged_and_working
                 )
 
             if (weighting_factors is not None) and (len(weighting_factors)==0):
                 weighting_factors = None
+            
+            if bugged_and_working:
+                print(f'Weighting factors are not used as bugged_and_working flag is {bugged_and_working}')
+                weighting_factors = None
+
 
             # samples from unlabelled pool predictions
             sampler(
